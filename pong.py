37f146eedb15b2e82b1e265b7a0d75f761547646
Filip Kozal <109867923+FilipK206@users.noreply.github.com>
2024-04-11 17:26:05 +0200

    Adds ball animation

--- pong.py
+++ pong.py
@@ -18,12 +18,25 @@ opponent = pygame.Rect(10, screen_height/2 - 70, 10, 140)
 
 background_color = pygame.Color('grey12')
 light_grey = (200, 200, 200)
+ball_speed_x = 7
+ball_speed_y = 7
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+
+    # moves from the start
+    ball.x += ball_speed_x
+    ball.y += ball_speed_y
+
+    # bouncing ball animation
+    if ball.left <= 0 or ball.right >= screen_width:
+        ball_speed_x *= -1
+    if ball.top <= 0 or ball.bottom >= screen_height:
+        ball_speed_y *= -1
+
     # static background graphic
     screen.fill(background_color)
     pygame.draw.aaline(screen, light_grey, (screen_width / 2, 0), (screen_width / 2, screen_height))
@@ -33,7 +46,7 @@ while running:
     pygame.draw.rect(screen, light_grey, player)
     pygame.draw.rect(screen, light_grey, opponent)
     pygame.draw.ellipse(screen, light_grey, ball)
-    
+
     pygame.display.flip()
 
     clock.tick(60)
