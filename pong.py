1cbee41355bd12790ed7121ada06386339df3aa1
Filip Kozal <109867923+FilipK206@users.noreply.github.com>
2024-04-12 12:42:23 +0200

    Calculates scores and adds labels

--- pong.py
+++ pong.py
@@ -11,8 +11,22 @@ def static_background():
     pygame.draw.aaline(screen, light_grey, (screen_width / 2, 0), (screen_width / 2, screen_height))
     pygame.draw.ellipse(screen, light_grey, [screen_width / 2 - 110, screen_height / 2 - 110, 220, 220], 2)
 
+    score_player_label = main_font.render(f"Score Player: {score_player}", 1, light_grey)
+    score_opponent_label = main_font.render(f"Score Opponent: {score_opponent}", 1, light_grey)
+
+    screen.blit(score_player_label, (screen_width - 150, 0 + 15))
+    screen.blit(score_opponent_label, (0 + 50, 0 + 15))
+def count_score_player():
+    global score_player
+    score_player += 1
+
+def count_score_opponent():
+    global score_opponent
+    score_opponent += 1
+
+
 def ball_animation():
-    global ball_speed_x, ball_speed_y
+    global ball_speed_x, ball_speed_y, score_opponent, score_player
     # moves from the start
     ball.x += ball_speed_x
     ball.y += ball_speed_y
@@ -24,8 +38,13 @@ def ball_animation():
         ball_speed_y *= -1
 
     # collisions with players rectangles
-    if ball.colliderect(player) or ball.colliderect(opponent):
+    if ball.colliderect(player):
         ball_speed_x *= -1
+        count_score_player()
+
+    if ball.colliderect(opponent):
+        ball_speed_x *= -1
+        count_score_opponent()
 
 def opponent_movement():
     if opponent.top < ball.y:
@@ -53,9 +72,11 @@ def ball_restart():
     ball_speed_y *= random.choice((1, -1))
 
 pygame.init()
+pygame.font.init()
 
 clock = pygame.time.Clock()
 
+main_font = pygame.font.SysFont("Bahnschrift", 15, bold=False)
 screen_width = 1080
 screen_height = 720
 screen = pygame.display.set_mode((screen_width, screen_height))
@@ -67,6 +88,9 @@ running = True
 ball = pygame.Rect(screen_width/2 - 10, screen_height/2 - 10, 20, 20)
 player = pygame.Rect(screen_width - 20, screen_height/2 - 70, 10, 140)
 opponent = pygame.Rect(10, screen_height/2 - 70, 10, 140)
+
+score_player = 0
+score_opponent = 0
 
 background_color = pygame.Color('grey12')
 light_grey = (200, 200, 200)
